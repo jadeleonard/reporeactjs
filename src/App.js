c10b9dd74cd8c3695d9bcc6b367a39232ee3001f
jadeleonard <lukabartolomeo9@gmail.com>
2023-11-27 13:21:02 -0800

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Hero from './components/hero'
+import Carousel from './components/carousel'
 
 const App = () => {
   return (
     <div>
-      <Link to='/dashboard'>Dashboard</Link>
+     
+
+
+     <Navbar />
+      <Hero />
     </div>
   )
 }
